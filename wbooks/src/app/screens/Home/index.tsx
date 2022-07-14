3fcc71087a7b0e7971c4e49c3187953f3a39ace5
lauuta185 <lautaro.a.lopez@accenture.com>
2022-07-14 15:11:02 -0300

    Fix typescript errors and make BookList component

--- wbooks/src/app/screens/Home/index.tsx
+++ wbooks/src/app/screens/Home/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Book from '@app/components/Book';
-import { BOOKS_MOCK } from '@constants/mockBooks';
 import { View } from 'react-native';
 import BookList from '@app/components/BookList';
 
