42a93d443848b5a285576989a97d1c6e66af5b7a
lauuta185 <lautaro.a.lopez@accenture.com>
2022-07-18 13:09:53 -0300

    Config react navigation and fix errors

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 
 import App from './src/app';
 
 export default function index() {
-  return (
-    <NavigationContainer>
-      <App />
-    </NavigationContainer>
-  );
+  return <App />;
 }
--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -11,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Home} options={{ title: 'Overview' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
