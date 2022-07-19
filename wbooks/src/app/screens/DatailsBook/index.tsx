908164eb0998a9b6e37f717dbc3474190e7cab3b
lauuta185 <lautaro.a.lopez@accenture.com>
2022-07-19 10:42:05 -0300

    React Navigation 1 and detailBook styles

---
+++ wbooks/src/app/screens/DatailsBook/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import DetailsBook from '@app/components/DetailsBook';
+
+import styles from './styles';
+
+function DetailBook() {
+  return (
+    <View style={styles.home}>
+      <DetailsBook />
+    </View>
+  );
+}
+export default DetailBook;
--- wbooks/src/app/screens/Home/index.tsx
+++ wbooks/src/app/screens/Home/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import BookList from '@app/components/BookList';
 
+import DetailBook from '../DatailsBook';
+
 import styles from './styles';
 
 function Home() {
   return (
     <View style={styles.home}>
       <BookList />
+      <DetailBook />
     </View>
   );
 }
