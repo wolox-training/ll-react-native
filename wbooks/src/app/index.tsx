9cc17e16e26b91856be3b50cd89a6f6ffd3cf5a4
lauuta185 <lautaro.a.lopez@accenture.com>
2022-07-29 12:58:57 -0300

    Adding Tap Bar

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,43 +1,73 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { white } from '@constants/colors';
+import { View, Text } from 'react-native';
 
 import { headerStyle, headerTitleStyle } from './styles';
 import Home from './screens/Home';
 import BookDetails from './screens/BookDetails';
 import BackButton from './components/BackButton';
 
-const RootStack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Library = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'HOME',
+          headerStyle,
+          headerTintColor: white,
+          headerTitleStyle,
+          headerBackTitleVisible: false
+        }}
+      />
+
+      <Stack.Screen
+        name="Details"
+        component={BookDetails}
+        options={{
+          title: 'BOOK DETAIL',
+          headerStyle,
+          headerTitleStyle,
+          headerTintColor: white,
+          headerLeft: () => <BackButton />
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Settings = () => {
+  return (
+    <View>
+      <Text>Hi, i am settings screen !</Text>
+    </View>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen
-          name="Home"
-          component={Home}
+      <Tab.Navigator>
+        <Tab.Screen name="Library" component={Library} options={{ headerShown: false }} />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
           options={{
-            title: 'HOME',
-            headerStyle,
-            headerTintColor: white,
-            headerTitleStyle
-          }}
-        />
-
-        <RootStack.Screen
-          name="Details"
-          component={BookDetails}
-          options={{
-            title: 'BOOK DETAIL',
+            title: 'SETTINGS',
             headerStyle,
             headerTitleStyle,
             headerTintColor: white,
-            headerBackTitleVisible: false,
             headerLeft: () => <BackButton />
           }}
         />
-      </RootStack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
--- wbooks/src/app/styles.ts
+++ wbooks/src/app/styles.ts
@@ -2,11 +2,8 @@ import { pictonBlue, white } from '@constants/colors';
 
 export const headerStyle = { backgroundColor: pictonBlue };
 export const headerTitleStyle = {
-  fontStyle: 'medium',
   fontSize: 20,
-  lineHeight: 22,
-  aling: 'right',
-  verticalAling: 'center'
+  lineHeight: 22
 };
 export const headerTintColor = { color: white };
 export const headerLeft = { marginLeft: 10 };
