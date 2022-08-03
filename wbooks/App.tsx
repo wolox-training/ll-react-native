56276dce5b518241bc45603bb4547a07219d0837
lauuta185 <lautaro.a.lopez@accenture.com>
2022-08-03 11:50:28 -0300

    Fix env errors

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
-import Config from 'react-native-config';
 
 import App from './src/app';
-
-const secretVariable = Config.API_URL;
-const secretKey = Config.GOOGLE_MAPS_API_KEY;
 
 export default function index() {
   return <App />;
