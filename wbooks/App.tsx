06d4873fe85882b410122f031a85a4b3647256a6
lauuta185 <lautaro.a.lopez@accenture.com>
2022-08-03 11:17:32 -0300

    react-native-config

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import Config from 'react-native-config';
 
 import App from './src/app';
+
+const secretVariable = Config.API_URL;
+const secretKey = Config.GOOGLE_MAPS_API_KEY;
 
 export default function index() {
   return <App />;
