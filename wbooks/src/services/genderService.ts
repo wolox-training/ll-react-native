1817526f069e77ce570d320231eb512c21542625
lauuta185 <lautaro.a.lopez@accenture.com>
2022-08-04 10:14:42 -0300

    Fix error, api is working now

--- wbooks/src/services/genderService.ts
+++ wbooks/src/services/genderService.ts
@@ -1,22 +1,9 @@
 import api from '@config/api';
 
-import reactotron from '../../ReactotronConfig';
-
 const genderService = () => api.get('?gender=female');
 
-const getGenres = async () => {
-  await genderService().then(response => response.data);
+const getByGenres = async () => {
+  await genderService().then(response => console.log(response.data.results));
 };
 
-export default getGenres;
-
-reactotron.apisauce(getGenres());
-api.addMonitor(reactotron.apisauce);
-
-/* const getGenres = async () => {
-  await api.get('?gender=female').then(response => {
-    response.data;
-  });
-};
-
-export default getGenres; */
+export default getByGenres;
